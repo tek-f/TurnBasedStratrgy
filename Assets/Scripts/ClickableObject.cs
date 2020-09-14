99602516d1de545034bfad36c09a83d2ba537a8f
tek-f <aaron.falco2@gmail.com>
2020-09-14 12:59:44 +1000

    Initial Commit

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    bool highlighted;
+
+    public void ToggleHighlight()
+    {
+        highlighted = !highlighted;
+        Debug.Log(highlighted);
+    }
+}
