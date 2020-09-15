047c9537525cd6fad831bf3abf2e5dad94a373cd
tek-f <aaron.falco2@gmail.com>
2020-09-15 16:30:52 +1000

    Working On Selecting Objects
    Also added namespace for units, AztecArmy.Units

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AztecArmy.Units;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static Unit selectedUnit;
+    int selectionState = 0;//used to determine what kind of selection state the player is in, i.e. selecting units, selecting where to move the selectedUnit
+
+    Camera mainCamera;
+    /*void SelectObject(GameObject clicked)
+    {
+        selectedUnit = clicked.GetComponent<Unit>();
+        selectedUnit.GetComponent<Unit>().OnSelection();
+    }*/
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    }
+    void Update()
+    {
+        switch (selectionState)
+            {
+                case 0://selecting units
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit))
+                        {
+                            if (hit.transform.gameObject.GetComponent<Unit>() != null)
+                            {
+                                
+                            }
+                            else
+                            {
+                                
+                            }
+                        }
+                    }
+                    break;
+                case 1://selecting movement
+
+                    break;
+                case 2://selecting attack target
+
+                    break;
+            }
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AztecArmy.Units
+{
+    public class Unit : MonoBehaviour
+    {
+        #region Variables
+        [Header("Game Management")]
+        bool selected;
+        [Header("Unit Metrics")]
+        int health;
+        #endregion
+        public void OnSelection()
+        {
+            //set units menu to active
+            //selected = true
+        }
+        public void Movement()
+        {
+
+        }
+        public void BasicAttack(int damage, int range, Unit target)
+        {
+
+        }
+    }
+}
